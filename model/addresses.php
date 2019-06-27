b5f309f52dac95f37a9fe7f54415043667a6f42b
yoelEdelman <yoeledelman@gmail.com>
2019-06-27 20:39:12 +0200

    Fonctionnalités adresse terminée

--- model/addresses.php
+++ model/addresses.php
@@ -1,76 +1,75 @@
 <?php
-
 require_once('dbConnect.php');
 
-    function addAddress($address, $zipCode, $city, $country, $location = FALSE)
-    {
-        $db = dbConnect();
-        $queryString = 'INSERT INTO addresses (address, zip_code, city ';
-        $queryValues = 'VALUES (:address, :zipCode, :city';
-        $queryParameters = [
-            'address' => htmlspecialchars($address),
-            'zipCode' => htmlspecialchars($zipCode),
-            'city' => htmlspecialchars($city)
-        ];
+function addAddress($address, $zipCode, $city, $country, $location = FALSE)
+{
+    $db = dbConnect();
+    $queryString = 'INSERT INTO addresses (address, zip_code, city ';
+    $queryValues = 'VALUES (:address, :zipCode, :city';
+    $queryParameters = [
+        'address' => htmlspecialchars($address),
+        'zipCode' => htmlspecialchars($zipCode),
+        'city' => htmlspecialchars($city)
+    ];
 
-        if (isset($country) && !empty($country)) {
-            $queryString .= ', country ';
-            $queryValues .= ', :country';
-            $queryParameters['country'] = htmlspecialchars($country);
-        }
-        elseif (isset($location) && !empty($location)) {
-            $queryString .= ', location ';
-            $queryValues .= ', :location';
-            $queryParameters['location'] = htmlspecialchars($location);
+    if (isset($country) && !empty($country)) {
+        $queryString .= ', country ';
+        $queryValues .= ', :country';
+        $queryParameters['country'] = htmlspecialchars($country);
+    }
+    elseif (isset($location) && !empty($location)) {
+        $queryString .= ', location ';
+        $queryValues .= ', :location';
+        $queryParameters['location'] = htmlspecialchars($location);
 
-        }
-
-        $queryString .= ') ';
-        $queryValues .= ')';
-        $queryString .= $queryValues;
-
-        $query = $db->prepare($queryString);
-
-        $query->execute($queryParameters);
-
-        return $lastInsert = $db->lastInsertId();
     }
 
-    function updateAddress($address, $zipCode, $city, $country, $location = FALSE, $id)
-    {
-        $db = dbConnect();
-        $queryString = 'UPDATE addresses SET address = :address, zip_code = :zipCode, city = :city ';
-        $queryParameters = [
-            'address' => htmlspecialchars($address),
-            'zipCode' => htmlspecialchars($zipCode),
-            'city' => htmlspecialchars($city)
-        ];
+    $queryString .= ') ';
+    $queryValues .= ')';
+    $queryString .= $queryValues;
 
-        if (isset($country) && !empty($country)) {
-            $queryString .= ', country = :country ';
-            $queryParameters['country'] = htmlspecialchars($country);
-        }
-        elseif (isset($location) && !empty($location)) {
-            $queryString .= ', location = :location ';
-            $queryParameters['location'] = htmlspecialchars($location);
+    $query = $db->prepare($queryString);
 
-        }
-        $queryString .= 'WHERE id = :id';
-        $queryParameters['id'] = htmlspecialchars($id);
+    $query->execute($queryParameters);
 
-        $query = $db->prepare($queryString);
-        $query->execute($queryParameters);
+    return $lastInsert = $db->lastInsertId();
+}
+
+function updateAddress($address, $zipCode, $city, $country, $location = FALSE, $id)
+{
+    $db = dbConnect();
+    $queryString = 'UPDATE addresses SET address = :address, zip_code = :zipCode, city = :city ';
+    $queryParameters = [
+        'address' => htmlspecialchars($address),
+        'zipCode' => htmlspecialchars($zipCode),
+        'city' => htmlspecialchars($city)
+    ];
+
+    if (isset($country) && !empty($country)) {
+        $queryString .= ', country = :country ';
+        $queryParameters['country'] = htmlspecialchars($country);
     }
+    elseif (isset($location) && !empty($location)) {
+        $queryString .= ', location = :location ';
+        $queryParameters['location'] = htmlspecialchars($location);
 
-    function deleteAddresses($id)
-    {
-        $db = dbConnect();
-        $queryString = 'DELETE FROM addresses';
-        $queryString .= ' WHERE id = :id';
-
-        $query = $db->prepare($queryString);
-
-        $query->bindParam(':id', $id, PDO::PARAM_INT);
-
-        $query->execute();
     }
+    $queryString .= 'WHERE id = :id';
+    $queryParameters['id'] = htmlspecialchars($id);
+
+    $query = $db->prepare($queryString);
+    $query->execute($queryParameters);
+}
+
+function deleteAddresses($id)
+{
+    $db = dbConnect();
+    $queryString = 'DELETE FROM addresses';
+    $queryString .= ' WHERE id = :id';
+
+    $query = $db->prepare($queryString);
+
+    $query->bindParam(':id', $id, PDO::PARAM_INT);
+
+    $query->execute();
+}
